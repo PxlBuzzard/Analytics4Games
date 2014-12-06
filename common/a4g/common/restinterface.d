beb69bb50982d14a599c76e84354b685da7caf34
Colden Cullen <ColdenCullen@gmail.com>
2014-12-06 12:38:43 -0500

    Added A4GWebInterface

--- common/a4g/common/restinterface.d
+++ common/a4g/common/restinterface.d
@@ -1,6 +1,19 @@
 module a4g.common.restinterface;
 
+import vibe.web.rest;
+import vibe.data.json;
+
+struct DataEntry
+{
+    string type;
+    Json payload;
+}
+
 interface A4GWebInterface
 {
-    
+    @method(HTTPMethod.POST)
+    void storeData( string key, DataEntry data );
+
+    @method(HTTPMethod.GET)
+    DataEntry[] getData( string key );
 }
